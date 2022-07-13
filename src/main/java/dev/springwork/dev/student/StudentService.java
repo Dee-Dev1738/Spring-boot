976c8e245dcfb594f56e5d1f1bb5fdb75477777b
Dwight Hill <83855447+Dee-Dev1738@users.noreply.github.com>
2022-07-13 16:58:07 -0400

    Repositories impl

--- src/main/java/dev/springwork/dev/student/StudentService.java
+++ src/main/java/dev/springwork/dev/student/StudentService.java
@@ -1,23 +1,22 @@
 package dev.springwork.dev.student;
 
-import java.time.LocalDate;
-import java.time.Month;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentService {
-    
-    public List<Student> getStudents(){
-		return List.of(
-			new Student(
-				1L,
-				"Marian",
-				"marian.jar@gmail.com",
-				21,
-				LocalDate.of(2000, Month.JANUARY, 5)
-			)
-		);
+public class StudentService  {
+	
+	private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+	public List<Student> getStudents(){
+		return studentRepository.findAll();
 	}
 }
